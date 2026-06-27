d7c6af53b51d5a54abd4165a648cc1893afc520b
Anurag Prajapati <prajapatianurag73240@gmail.com>
2026-06-27 17:13:33 +0530

    refactor: sidebar ui

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,10 +1,9 @@
 import { SidebarOption } from "./SidebarOption";
-
 export const Sidebar = () => {
-    return <div className='fixed top-12 left-0 h-[calc(100vh-3rem)] w-64 border-r bg-white p-1'>
-        <SidebarOption path="/dashboard" name="Dasbhoard" />
-        <SidebarOption path="/emails" name="Emails" />
-        <SidebarOption path="/template" name="Templates" />
-        <SidebarOption path="/credentials" name="Credentials" />
+    return <div className='fixed top-12 left-0 h-[calc(100vh-3rem)]  p-2 w-50 border-r bg-white'>
+        <SidebarOption path="/dashboard" name="Dasbhoard" iconClass="fa-solid fa-chart-simple" />
+        <SidebarOption path="/emails" name="Emails" iconClass="fa-solid fa-envelope" />
+        <SidebarOption path="/template" name="Templates" iconClass="fa-solid fa-folder-tree" />
+        <SidebarOption path="/credentials" name="Credentials" iconClass="fa-solid fa-key" />
     </div>
 };
--- frontend/src/components/SidebarOption.tsx
+++ frontend/src/components/SidebarOption.tsx
@@ -1,8 +1,21 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
-export const SidebarOption = (props: { name: string, path: string }) => {
-    const { name, path } = props
-    return <main className="flex cursor-pointer  hover:bg-gray-300 hover:border hover:border-gray-600 justify-start text-start p-2">
-        <Link to={path} >{name}</Link >
-    </main>
+export const SidebarOption = (props: { name: string; path: string; iconClass?: string }) => {
+    const { name, path, iconClass } = props;
+    const location = useLocation();
+
+    const isActive = location.pathname === path;
+
+    return (
+        <Link
+            to={path}
+            key={name}
+            id={name}
+            className={`flex cursor-pointer justify-start text-start py-2 px-4 items-center mb-1
+        ${isActive ? "text-gray-600 bg-gray-300 border border-gray-600" : "text-gray-600 hover:bg-gray-300 hover:border hover:border-gray-600"}`}
+        >
+            {iconClass && <i className={iconClass}></i>}
+            <span className="ml-2">{name}</span>
+        </Link>
+    );
 };
