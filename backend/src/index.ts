7b57b0737e758ef88dea1c7fd721d858df5c9a13
Anurag Prajapati <prajapatianurag73240@gmail.com>
2026-07-04 14:13:28 +0530

    fix: cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,7 +14,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://notivo-q59p1jej0-anurag-prajapatis-projects.vercel.app",
+      "http://notivo-q59p1jej0-anurag-prajapatis-projects.vercel.app/",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: [
