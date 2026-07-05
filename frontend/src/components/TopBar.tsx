b68330396712569c66f39367732d0292bd2581bc
Anurag Prajapati <prajapatianurag73240@gmail.com>
2026-07-05 14:21:13 +0530

    fix: import

--- frontend/src/components/TopBar.tsx
+++ frontend/src/components/TopBar.tsx
@@ -1,4 +1,4 @@
-import { ChevronDown, LogOut, Moon, Settings, Zap } from "lucide-react"
+import { ChevronDown, LogOut, Settings, Zap } from "lucide-react"
 import { useEffect, useRef, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { useAuthContext } from "../hooks"
